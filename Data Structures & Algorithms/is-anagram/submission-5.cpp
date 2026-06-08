3e2b350b76eda50713b4e35aef44a9b4d4336706
sukirti123t <sukirtikanodia21@gmail.com>
2026-06-08 20:57:08 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.cpp
@@ -0,0 +1,27 @@
+#include <unordered_map>
+#include <string>
+using namespace std;
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if (s.length() != t.length())
+            return false;
+
+        unordered_map<char, int> freq;
+
+        for (char c : s) {
+            freq[c]++;
+        }
+
+        for (char c : t) {
+            freq[c]--;
+
+            if (freq[c] < 0) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+};
